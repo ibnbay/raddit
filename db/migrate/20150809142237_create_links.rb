56c9a2cc49e5c7d0749c646ed3954a1227768409
Ibnu Bayhaqi <sky89@live.com>
2015-08-10 12:18:14 +0000

    add assosiation linkand user

--- db/migrate/20150809142237_create_links.rb
+++ db/migrate/20150809142237_create_links.rb
@@ -1,7 +1,7 @@
 class CreateLinks < ActiveRecord::Migration
   def change
     create_table :links do |t|
-      t.string :titile
+      t.string :title
       t.string :url
 
       t.timestamps null: false
